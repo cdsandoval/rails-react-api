b2b9856cae7dfc6efa7aec4a8a581d5a5df74407
Kattya Cuevas <kattya.cuevas@gmail.com>
2019-06-28 10:32:19 -0500

    Update callback calculate price in order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,14 +5,15 @@ class Order < ApplicationRecord
   has_many :order_items
   accepts_nested_attributes_for :order_items
 
-  before_save :calculate_price
+  after_save :calculate_price, unless: ->(order) { order.total_price? }
 
   private
 
   def calculate_price
     order_items_list = order_items.includes(:menu_item)
-    self.total_price = order_items_list.reduce(0) do |sum, order_item|
+    result = order_items_list.reduce(0) do |sum, order_item|
       sum + order_item.menu_item.price * order_item.quantity
     end
+    update(total_price: result)
   end
 end
